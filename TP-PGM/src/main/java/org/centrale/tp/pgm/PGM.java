82083dbc079ca78edb46bc0a5aceec2a4b1fa9ec
LocPat <loic.patigny@yahoo.fr>
2022-12-01 11:17:51 +0100

    methode lire
    premiere version de la methode lire

--- TP-PGM/src/main/java/org/centrale/tp/pgm/PGM.java
+++ TP-PGM/src/main/java/org/centrale/tp/pgm/PGM.java
@@ -4,33 +4,51 @@
  */
 package org.centrale.tp.pgm;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * 
  * @author loicpatigny
  */
 public class PGM {
-    String P2;
+    String type;
     String commentaire;
     int largeur;
     int hauteur;
     int gris = 255;
+    List<List<Integer>> contenu;
+    
 
-    // Constructor
-    public PGM(String P2, String commentaire, int largeur, int hauteur) {
-        this.P2 = P2;
+    // Constructeurs
+
+    public PGM() {
+    }
+    
+    
+
+    public PGM(String type, String commentaire, int largeur, int hauteur, List<List<Integer>> contenu) {
+        this.type = type;
         this.commentaire = commentaire;
         this.largeur = largeur;
         this.hauteur = hauteur;
+        this.contenu = contenu;
     }
+    
+    
     
     //Getters and Setters
 
-    public String getP2() {
-        return P2;
+    public String getType() {
+        return type;
     }
 
-    public void setP2(String P2) {
-        this.P2 = P2;
+    public void setType(String P2) {
+        this.type = type;
     }
 
     public String getCommentaire() {
@@ -69,13 +87,45 @@ public class PGM {
     // Methodes
     
     /**
-     * 
+     * La fonction lire permet de lire un fichier .pgm comme un fichier texte et créer l'objet comme un PGM pour pouvoir effectuer des manipulations avec.
+     * @param nomFichier le nom du fichier dans lequel se situe l'image PGM
      */
-    public void readPGM(){
+    //FIXME gerer la matrice de pixels
+    public void lire(String nomFichier){
+        try{
+            //lecture du fichier
+            FileInputStream file = new FileInputStream(nomFichier);
+            Scanner scanner = new Scanner(file);
+            
+            //creation et remplissage d'un objet PGM
+            this.setType(scanner.nextLine());
+            this.setCommentaire(scanner.nextLine());
+            String[] str = scanner.nextLine().split(" ");
+            this.setLargeur(Integer.valueOf(str[0]));
+            this.setHauteur(Integer.valueOf(str[1]));
+            this.setGris(Integer.valueOf(scanner.nextLine()));
+            
+            //gestion de la matrice de pixels
+            int j = 0;
+            while (j < this.getHauteur()) {
+                int i = 0;
+                List ligne = new ArrayList<Integer>(this.getLargeur());
+                while (i < this.getLargeur()){
+                    for (String pixel : scanner.nextLine().split(" ")){
+                        ligne.add(Integer.valueOf(pixel));
+                        i+=1;
+                    }
+                }
+                this.contenu.add(ligne);
+            }
+            
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(PGM.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
     }
     
-    public void histogrammePGM(){
+    public void histogramme(){
         
     }
     
@@ -95,4 +145,6 @@ public class PGM {
     public void reduction(){
         
     } 
+
+
 }
