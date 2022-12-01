2fb939f8d118b111475fbc3df9d0a02b78c6aa65
LocPat <loic.patigny@yahoo.fr>
2022-12-01 09:37:12 +0100

    Squelette

--- TP-PGM/src/main/java/org/centrale/tp/pgm/PGM.java
+++ TP-PGM/src/main/java/org/centrale/tp/pgm/PGM.java
@@ -5,7 +5,7 @@
 package org.centrale.tp.pgm;
 
 /**
- *
+ * 
  * @author loicpatigny
  */
 public class PGM {
@@ -14,5 +14,70 @@ public class PGM {
     int largeur;
     int hauteur;
     int gris = 255;
+
+    // Constructor
+    public PGM(String P2, String commentaire, int largeur, int hauteur) {
+        this.P2 = P2;
+        this.commentaire = commentaire;
+        this.largeur = largeur;
+        this.hauteur = hauteur;
+    }
+    
+    //Getters and Setters
+
+    public String getP2() {
+        return P2;
+    }
+
+    public void setP2(String P2) {
+        this.P2 = P2;
+    }
+
+    public String getCommentaire() {
+        return commentaire;
+    }
+
+    public void setCommentaire(String commentaire) {
+        this.commentaire = commentaire;
+    }
+
+    public int getLargeur() {
+        return largeur;
+    }
+
+    public void setLargeur(int largeur) {
+        this.largeur = largeur;
+    }
+
+    public int getHauteur() {
+        return hauteur;
+    }
+
+    public void setHauteur(int hauteur) {
+        this.hauteur = hauteur;
+    }
+
+    public int getGris() {
+        return gris;
+    }
+
+    public void setGris(int gris) {
+        this.gris = gris;
+    }
+    
+    
+    // Methodes
+    
+    /**
+     * 
+     */
+    public void readPGM(){
+        
+    }
+    
+    public void histogrammePGM(){
+        
+    }
+    
     
 }
