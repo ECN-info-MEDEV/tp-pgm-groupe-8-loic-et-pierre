fabbf0c973a56afe4423218b86f32457edb0b07c
LocPat <loic.patigny@yahoo.fr>
2022-12-01 09:31:16 +0100

    PGM class

---
+++ TP-PGM/src/main/java/org/centrale/tp/pgm/PGM.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.centrale.tp.pgm;
+
+/**
+ *
+ * @author loicpatigny
+ */
+public class PGM {
+    String P2;
+    String commentaire;
+    int largeur;
+    int hauteur;
+    int gris = 255;
+    
+}
